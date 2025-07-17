84514fb472d3565becf3d765fdfd9824a73b0519
dopamine555 <dopaminelens13@gmail.com>
2025-07-17 17:32:13 -0400

    Improve HTML handling with proper head and body extraction

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
+import Head from 'next/head';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width" />
+        <meta name="generator" content="Framer 6aebf05" />
+        <script async src="https://events.framer.com/script?v=2" data-fid="ff2aee2c258bbbc8fd4a8334d9b76a0b47bfb5bdc8cc1c1229f7c149b4fbb035" data-no-nt></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/waitlist.js
+++ pages/waitlist.js
@@ -1,9 +1,24 @@
 import fs from 'fs';
 import path from 'path';
+import Head from 'next/head';
 
 export default function Waitlist({ content }) {
+  // Extract the body content from the HTML
+  const bodyContent = content.match(/<body[^>]*>([\s\S]*)<\/body>/i)?.[1] || '';
+
+  // Extract head content (title, meta tags, etc.)
+  const headContent = content.match(/<head[^>]*>([\s\S]*)<\/head>/i)?.[1] || '';
+  const title = headContent.match(/<title[^>]*>([\s\S]*?)<\/title>/i)?.[1] || 'CapturGO Waitlist';
+  const description = headContent.match(/<meta[^>]*name="description"[^>]*content="([^"]*)"/i)?.[1];
+
   return (
-    <div dangerouslySetInnerHTML={{ __html: content }} />
+    <>
+      <Head>
+        <title>{title}</title>
+        {description && <meta name="description" content={description} />}
+      </Head>
+      <div dangerouslySetInnerHTML={{ __html: bodyContent }} />
+    </>
   );
 }
 
