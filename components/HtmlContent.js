2081083e9b224db62094f3ff43cbdd14916b10bc
dopamine555 <dopaminelens13@gmail.com>
2025-07-17 17:24:47 -0400

    Simplify to use Next.js rewrites for clean URLs

--- components/HtmlContent.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-export function getHtmlContent(filePath) {
-  const fullPath = path.join(process.cwd(), 'public', filePath);
-  const fileContents = fs.readFileSync(fullPath, 'utf8');
-  return fileContents;
-}
